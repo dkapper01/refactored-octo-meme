f2580995af7d718e06d44fa679969e3e08c69dcc
Daniel Kapper <dkapper01@gmail.com>
2024-11-02 17:24:45 -0400

    change to dev branch

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -5,7 +5,7 @@ export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
 export default function Index() {
 	return (
 		<main className="">
-			<p>This is the home page</p>
+			<p>This is the home page dev</p>
 		</main>
 	)
 }
