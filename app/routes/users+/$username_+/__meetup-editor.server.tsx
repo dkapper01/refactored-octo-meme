4dfec9b3bd7081914c2b35420b650adf674f720c
Daniel Kapper <dkapper01@gmail.com>
2024-12-30 21:13:09 -0500

    add a user to meetups

--- app/routes/users+/$username_+/__meetup-editor.server.tsx
+++ app/routes/users+/$username_+/__meetup-editor.server.tsx
@@ -1,9 +1,11 @@
 import { parseWithZod } from '@conform-to/zod'
 import { json, type ActionFunctionArgs } from '@remix-run/node'
+import { requireUserId } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { formSchema } from './__meetup-editor'
 
 export async function action({ request }: ActionFunctionArgs) {
+	const userId = await requireUserId(request)
 	const formData = await request.formData()
 	const submission = parseWithZod(formData, { schema: formSchema })
 
@@ -15,7 +17,10 @@ export async function action({ request }: ActionFunctionArgs) {
 	}
 
 	const meetup = await prisma.meetup.create({
-		data: submission.value,
+		data: {
+			...submission.value,
+			ownerId: userId,
+		},
 	})
 
 	return json({ result: meetup })
