cba35b822c916917159a8f98be480fe4494d2a15
Daniel Kapper <dkapper01@gmail.com>
2025-02-05 21:36:27 -0500

    See if deployment will work with this code

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,29 +1,33 @@
-import { type MetaFunction } from '@remix-run/node'
-import ExploreEvents from '#app/components/explore-events.tsx'
+import { json, type MetaFunction } from '@remix-run/node'
+// import ExploreEvents from '#app/components/explore-events.tsx'
 // import { Link, useLocation } from '@remix-run/react'
 
 // import { Icon } from '#app/components/ui/icon.tsx'
-
-// import { prisma } from '#app/utils/db.server.ts'
+import { useLoaderData } from '@remix-run/react'
+import { prisma } from '#app/utils/db.server.ts'
 
 export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
 
-// export async function loader() {
-// 	const meetups = await prisma.meetup.findMany()
-// 	return json({ meetups })
-// }
+export async function loader() {
+	const meetups = await prisma.meetup.findMany()
+	return json({ meetups })
+}
 
 export default function Index() {
 	// const currentView = 'map'
 	// const location = useLocation()
 
 	// const navigation = useNavigation()
-	// const data = useLoaderData<typeof loader>()
+	const data = useLoaderData<typeof loader>()
+	console.log(data)
 
 	return (
 		<div className="relative flex flex-col text-gray-800">
 			<main className="container fixed left-0 right-0 top-0 mt-20">
-				<ExploreEvents />
+				{/* <ExploreEvents /> */}
+				{data?.meetups?.map((meetup) => (
+					<div key={meetup.id}>{meetup.title}</div>
+				))}
 			</main>
 		</div>
 	)
